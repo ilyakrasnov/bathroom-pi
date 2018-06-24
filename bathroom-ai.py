9333bd922b9efca7e198649d6a0207d9368a34ea
Ilya <ilya.krasnov@gmail.com>
2018-06-24 18:25:44 +0300

    Refactor with requests

--- bathroom-ai.py
+++ bathroom-ai.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO #import the GPIO library
 import time
-import urllib
+import requests
 import json
 
-URL = 'http://bathroom-ai.herokuapp.com/api/rooms'
+BASE_URL = "https://bathroom-ai.herokuapp.com/api/rooms"
+CHANGE_URL = BASE_URL + "/%s/%s"
 FEMALE = 8
 MALE = 10
 
@@ -15,43 +16,45 @@ GPIO.setup(FEMALE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(MALE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-response = json.loads(urllib.urlopen(URL).read())
-result = dict(zip([x['id'] for x in response], [x['occupied'] for x in response]))
+response = requests.get(BASE_URL)
+result = dict(zip([x['id'] for x in r.json()], [x['occupied'] for x in r.json()]))
 
 femaleDoorOccupied = result[ROOM_F_ID]
 maleDoorOccupied = result[ROOM_M_ID]
 
-def free_room(id):
-    urllib.urlopen(URL + '/' + str(id) + '/free').read()
-    
+def change_room_status(id, action):
+     requests.post(CHANGE_URL % (id, action))
 
-def occupy_room(id):
-    urllib.urlopen(URL + '/' + str(id) + '/occupy').read()
+def free(id):
+    change_room_status(id, FREE_ACTION)
+
+def occupy(id):
+    change_room_status(id, OCCUPY_ACTION)
 
 while True:
     if GPIO.input(FEMALE):
         print("Door 1 is open")
         if femaleDoorOccupied:
             femaleDoorOccupied = False
-            free_room(ROOM_F_ID)
+            free(ROOM_F_ID)
         time.sleep(2)
     if GPIO.input(FEMALE) == False:
         print("Door 1 is closed")
         if not femaleDoorOccupied:
             femaleDoorOccupied = True
-            occupy_room(ROOM_F_ID)  
+            occupy(ROOM_F_ID)
         time.sleep(2)
     if GPIO.input(MALE):
         print("Door 2 is open")
         if maleDoorOccupied:
             maleDoorOccupied = False
-            free_room(ROOM_M_ID)
+            free(ROOM_M_ID)
         time.sleep(2)
     if GPIO.input(MALE) == False:
         print("Door 2 is closed")
         if not maleDoorOccupied:
             maleDoorOccupied = True
-            occupy_room(ROOM_M_ID)  
+            occupy(ROOM_M_ID)
         time.sleep(2)
 
 
